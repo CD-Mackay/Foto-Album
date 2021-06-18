c9913f81245372fd297c54def9a5468267527f8f
Connor Mackay <71985737+CD-Mackay@users.noreply.github.com>
2021-06-18 15:17:50 -0400

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+const htmlPlugin = new HtmlWebpackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+})
+
+module.exports = {
+  entry: "./src",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        type: "asset",
+        parser: { dataUrlCondition: { maxSize: 15000 } },
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ["*", ".js", ".jsx", '.scss'],
+  },
+  plugins: [
+    htmlPlugin,
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? '[name].css' : '[name].[hash].css',
+      chunkFilename: isDevelopment ? '[id].css' : '[id].[hash].css'
+    })
+  ]
+};
