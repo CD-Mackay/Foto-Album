9c6eea641db31f9efe91dd33bb5c815903a227f5
Connor Mackay <connor.mackay@gmail.com>
2021-06-18 16:00:03 -0400

    create inputfield component

---
+++ src/components/elements/InputField/InputField.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import StyledInputField from './StyledInputField.js';
+
+export default function InputField(props) {
+  return (
+    <StyledInputField>
+      <p>I am input field</p>
+    </StyledInputField>
+  )
+};
