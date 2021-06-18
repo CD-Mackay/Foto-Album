531bdd1ad1579be4451696cef1fa55d3753a62b7
Connor Mackay <connor.mackay@gmail.com>
2021-06-18 15:58:47 -0400

    create styledlabel component

--- src/components/elements/Label/Label.js
+++ src/components/elements/Label/Label.js
@@ -1,1 +1,10 @@
 import React from 'react';
+import StyledLabel from './StyledLabel.js';
+
+export default function Label(props) {
+  return (
+    <StyledLabel>
+      <p>{props}</p>
+    </StyledLabel>
+  )
+};
